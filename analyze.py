5aadba47a25601d7707f22d8ba055a08fffb030d
Benjamin Li <benjaminli@hotmail.ca>
2024-11-13 22:31:05 -0500

    reorganized files

--- analyze.py
+++ analyze.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
 # Set up data frames
 
--- stroopanalysis.R
+++ stroopanalysis.R
@@ -1,4 +1,4 @@
-# linear mixed models for stroop and emotional stroop analysis 
+# linear mixed models for stroop and emotional stroop analysis
 
 library(lme4)
 library(simr)
@@ -7,44 +7,48 @@ library(tidyverse)
 set.seed(1234)
 
 # initialize dataframes
-df <- read.csv("C:/CodingStuff/Thesis_Rumination_Stroop/FinalData/stroop_standard_trials.csv")
-df2 <- read.csv("C:/CodingStuff/Thesis_Rumination_Stroop/FinalData/stroop_emotion_trials.csv")
- 
+df <- read.csv("stroop_standard_trials.csv")
+df2 <- read.csv("stroop_emotion_trials.csv")
+
 # rrs standard trials
 m <- lmer(
-   mean_RT ~ counterbalancing_group*congruency*RRS_group*age*sex
-   + (1|prolific_ID),
-   data=df)
+  mean_RT ~ counterbalancing_group * congruency * RRS_group * age * sex
+    + (1 | prolific_ID),
+  data = df
+)
 
 # save table to html
-tab_model(m, file = 'rrs_standard_trials.doc') 
+tab_model(m, file = "rrs_standard_trials.doc")
 
 
 # rrs emotion trials
 m2 <- lmer(
-  mean_RT ~ counterbalancing_group*valence*RRS_group*age*sex
-  + (1|prolific_ID),
-  data=df2)
+  mean_RT ~ counterbalancing_group * valence * RRS_group * age * sex
+    + (1 | prolific_ID),
+  data = df2
+)
 
-tab_model(m2, file = 'rrs_emotion_trials.html') #, file = "blahblah.doc") #change this so it saves to your desired directory
+tab_model(m2, file = "rrs_emotion_trials.html") # , file = "blahblah.doc") #change this so it saves to your desired directory
 
 # bsri standard trials
 m3 <- lmer(
-  mean_RT ~ counterbalancing_group*congruency*BSRI_group*age*sex
-  + (1|prolific_ID),
-  data=df)
+  mean_RT ~ counterbalancing_group * congruency * BSRI_group * age * sex
+    + (1 | prolific_ID),
+  data = df
+)
 
-tab_model(m3, file = 'bsri_standard_trials.html')
+tab_model(m3, file = "bsri_standard_trials.html")
 
 # bsri emotion trials
 m4 <- lmer(
-  mean_RT ~ counterbalancing_group*valence*BSRI_group*age*sex
-  + (1|prolific_ID),
-  data=df2)
+  mean_RT ~ counterbalancing_group * valence * BSRI_group * age * sex
+    + (1 | prolific_ID),
+  data = df2
+)
 
-tab_model(m4, file = 'bsri_emotion_trials.html')
+tab_model(m4, file = "bsri_emotion_trials.html")
 
 
-# sumtable <- summary(m)
-# plot_model(m)
-# plot_model(m, type="est")
+sumtable <- summary(m)
+plot_model(m)
+plot_model(m, type = "est")
